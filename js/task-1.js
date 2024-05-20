becd15efd0780c04546de12ef85966f3fe05335d
Kateryna <mweoliss74@gmail.com>
2024-05-20 16:49:23 +0300

    g

---
+++ js/task-1.js
@@ -0,0 +1,17 @@
+
+
+const listElem = document.querySelectorAll('.item');
+
+// const res = listElem.length;
+// console.log(`Number of categories: ${res}`);
+
+// listElem.forEach(elem => {
+    
+//     const categoryTitle = elem.querySelector('h2').textContent;
+
+//     const categoryElements = elem.querySelector('ul li');
+
+
+//     console.log(`Category: ${categoryTitle}`);
+//     console.log(`Elements: ${categoryElements.length}`);
+// });
